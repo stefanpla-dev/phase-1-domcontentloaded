82abcf99777725691c86fca08a32f6db7d40369b
Stefan Pla <spla@icloud.com>
2024-02-09 14:13:28 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded.");
+
+    const changeText = document.querySelector("#text")
+    changeText.textContent = "This is really cool!";
+})
+
+console.log("This one will probably appear first.");
